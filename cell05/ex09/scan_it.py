703f5a2b7ee924534b140fb464f7af89273ec686
Taechit W. <twongsopha47@gmail.com>
2026-09-24 16:29:17 +0700

    Create scan_it.py

---
+++ cell05/ex09/scan_it.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import sys
+import re
+
+if len(sys.argv) != 3:
+    print("none")
+else:
+    keyword = sys.argv[1]
+    search_string = sys.argv[2]
+    
+    matches = re.findall(keyword, search_string)
+    
+    if matches:
+        print(len(matches))
+    else:
+        print("none")
